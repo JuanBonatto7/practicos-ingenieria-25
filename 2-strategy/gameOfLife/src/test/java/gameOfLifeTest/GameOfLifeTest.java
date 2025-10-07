5705a5f121c18e9abc667ee51a9a9717a5e9e18d
Tanox09 <juanbonatto7@gmail.com>
2025-10-07 16:46:14 -0300

    Update GameOfLifeTest.java

--- 2-strategy/gameOfLife/src/test/java/gameOfLifeTest/GameOfLifeTest.java
+++ 2-strategy/gameOfLife/src/test/java/gameOfLifeTest/GameOfLifeTest.java
@@ -3,7 +3,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
+import gameOfLife.Cell;
 import gameOfLife.ClasicRules;
+import gameOfLife.ClassicColorStrategy;
+import gameOfLife.Color;
+import gameOfLife.ColorStrategy;
 import gameOfLife.GameOfLife;
 import gameOfLife.NewRules;
 
@@ -11,36 +15,53 @@ public class GameOfLifeTest {
     
     @Test
     public void celulaSinVecinos(){
-        GameOfLife game = new GameOfLife(3,3, new ClasicRules());
-        game.setCelula(1,1);
+        GameOfLife game = new GameOfLife(3,3, new ClasicRules(), new ClassicColorStrategy());
+        ColorStrategy color = game.getColorStrategy();
+        game.setCell(1,1,color);
 
         game.nextState();
-        boolean cel = game.getCelula(1,1);
-        assertFalse(cel);
+        Cell cel = game.getCell(1,1);
+        assertFalse(cel.isAlive());
     }
 
     @Test
     public void celulaConVecinos(){
-        GameOfLife game = new GameOfLife(3,3, new ClasicRules());
-        game.setCelula(1,1);
-        game.setCelula(2,1);
-        game.setCelula(1,2);
+        GameOfLife game = new GameOfLife(3,3, new ClasicRules(), new ClassicColorStrategy());
+        ColorStrategy color = game.getColorStrategy();
+        game.setCell(1,1,color);
+        game.setCell(2,1,color);
+        game.setCell(1,2,color);
 
         game.nextState();
-        boolean cel = game.getCelula(2,2);
-        assertTrue(cel);
+        Cell cel = game.getCell(2,2);
+        assertTrue(cel.isAlive());
     }
 
     @Test
     public void newRulesTest(){
-        GameOfLife game = new GameOfLife(10,10, new NewRules());
-        game.setCelula(1,1);
-        game.setCelula(2,1);
-        game.setCelula(1,2);
+        GameOfLife game = new GameOfLife(3,3, new ClasicRules(), new ClassicColorStrategy());
+        ColorStrategy color = game.getColorStrategy();
+        game.setCell(1,1,color);
+        game.setCell(2,1,color);
+        game.setCell(1,2,color);
 
         game.nextState();
-        boolean cel = game.getCelula(2,2);
-        assertTrue(cel);
+        Cell cel = game.getCell(2,2);
+        assertTrue(cel.isAlive());
+    }
+
+    @Test
+    public void ColorStrategy(){
+        GameOfLife game = new GameOfLife(3,3, new ClasicRules(), new ClassicColorStrategy());
+        ColorStrategy color = game.getColorStrategy();
+        game.setCell(1,1,color);
+        game.setCell(2,1,color);
+        game.setCell(1,2,color);
+
+        game.nextState();
+        Cell cel = game.getCell(2,2);
+        
+        assert("WHITE", cel.getColor());
     }
 
 }
